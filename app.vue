4ddb7f2cdef508a5ddac2a927446aa39e80d4b1d
Maksim Kokoshihin <kokoshicha30@gmail.com>
2023-11-30 17:30:57 +0300

    project setup

--- app.vue
+++ app.vue
@@ -43,6 +43,14 @@ useHead({
 @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,500;6..12,600;6..12,700&family=Rubik:wght@400;500;600&display=swap'); // Nunito Sans
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Rubik:wght@400;500;600&display=swap'); // Roboto
 
+h3 {
+  font-family: Inter;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
 h4 {
   font-family: 'Nunito Sans', sans-serif;
   font-size: 16px;
